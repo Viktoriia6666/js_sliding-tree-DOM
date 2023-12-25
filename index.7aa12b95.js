c5df04ee695fd2a6523658fbd50c75c9de3274e9
Viktoriia Tkachyk <viko66666vs@gmail.com>
2023-12-25 22:03:13 +0200

    make build

---
+++ index.7aa12b95.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");t.id="listItem",e.prepend(t),t.append(t.nextSibling)}),e.addEventListener("click",function(e){if("listItem"===e.target.id){var t=e.target.parentNode.querySelector("ul");t&&(t.hidden=!t.hidden)}});//# sourceMappingURL=index.7aa12b95.js.map
+
+//# sourceMappingURL=index.7aa12b95.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const list = document.querySelector(".tree");
+const listItems = document.querySelectorAll("li");
+listItems.forEach((item)=>{
+    const span = document.createElement("span");
+    span.id = "listItem";
+    item.prepend(span);
+    span.append(span.nextSibling);
+});
+list.addEventListener("click", (e)=>{
+    if (e.target.id === "listItem") {
+        const header = e.target.parentNode.querySelector("ul");
+        header && (header.hidden = !header.hidden);
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
